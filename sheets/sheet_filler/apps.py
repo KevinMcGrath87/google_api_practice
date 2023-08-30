6dd889e4582f8c3079ec7e403410b195efc154dd
Kevin McGrath <kpmcgra@gmail.com>
2023-08-30 15:50:25 -0500

    removed unnecessary files and directories

--- sheets/sheet_filler/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SheetFillerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sheet_filler'
--- sheets/sheet_filler/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Cell(models.Model):
-    cell_address = models.CharField(max_length=2)
-    cell_value = models.TextField
-    
